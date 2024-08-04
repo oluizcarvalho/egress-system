e8ae70b46fd2a06072799cae272fb07e02f1409c
Luiz Carvalho <luizandre.ita@gmail.com>
2024-08-04 10:55:58 -0300

    (fix): menu redirect

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,12 +1,13 @@
-import { afterNextRender, AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { afterNextRender, Component } from '@angular/core';
 import { LIST_MENU } from './const/list-menu';
 import { NgClass, NgTemplateOutlet } from '@angular/common';
 import BRMenu from '@govbr-ds/core/dist/components/menu/menu';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-menu',
 	standalone: true,
-	imports: [NgClass, NgTemplateOutlet],
+	imports: [NgClass, NgTemplateOutlet, RouterLink],
 	templateUrl: './menu.component.html',
 	styleUrl: './menu.component.scss',
 })
