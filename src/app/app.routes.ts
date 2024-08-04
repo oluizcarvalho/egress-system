b37b72a586fc31119a6d397755c83c6ccb652cf0
Luiz Carvalho <luizandre.ita@gmail.com>
2024-08-04 10:52:07 -0300

    (feat): not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{ path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{
+		path: 'home',
+		loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+		title: 'Home | Sistema De Egressos',
+		data: {
+			breadCrumb: 'Home',
+		},
+	},
+	{
+		path: '**',
+		loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent),
+		title: 'Página Não Encontrada | Sistema De Egressos',
+		data: {
+			breadCrumb: 'Página Não Encontrada',
+		},
+	},
 ];
