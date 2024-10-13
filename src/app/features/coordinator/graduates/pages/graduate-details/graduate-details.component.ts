961d5e29b921f93717ae5d23a85f071320cbcc42
Luiz Carvalho <luizandre.ita@gmail.com>
2024-10-13 18:22:29 -0300

    (chore): refactor imports

--- src/app/features/coordinator/graduates/pages/graduate-details/graduate-details.component.ts
+++ src/app/features/coordinator/graduates/pages/graduate-details/graduate-details.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Tabs, TabsComponent } from '@shared/components/tabs/tabs.component';
-import { PersonalDataComponent } from '@app/features/coordinator/graduates/components/personal-data/personal-data.component';
-import { AcademicInformationComponent } from '@app/features/coordinator/graduates/components/academic-information/academic-information.component';
-import { ProfessionalInformationComponent } from '@app/features/coordinator/graduates/components/professional-information/professional-information.component';
-import { TestimonialsComponent } from '@app/features/coordinator/graduates/components/testimonials/testimonials.component';
-import { PublicationsComponent } from '@app/features/coordinator/graduates/components/publications/publications.component';
+import {
+	AcademicInformationComponent,
+	PersonalDataComponent,
+	ProfessionalInformationComponent,
+	PublicationsComponent,
+	TestimonialsComponent,
+} from '@features/coordinator/graduates/components';
 
 export enum GraduateTabs {
 	PersonalData = 0,
